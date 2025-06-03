e6e210a6e9b1af8f88e5199ff63881f67b8f21b1
Chabz <146414097+VursaZar@users.noreply.github.com>
2025-06-03 22:46:44 +0300

    Delete SCANMASTER directory

--- SCANMASTER/js/headerBuy.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const icon = document.getElementById("cartIcon");
-    if (!icon) return;
-  
-    const basket = JSON.parse(localStorage.getItem("basket") || "[]");
-    icon.style.display = basket.length > 0 ? "inline-flex" : "none";
-  });
-  
